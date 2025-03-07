65d1217acf7b3b5ca84e5fe7cb0d17b1c34fb2ca
Nilesh Sarkar <sarkarnilesh334@gmail.com>
2025-03-07 16:33:18 +0530

    7-13-25

---
+++ controllers/controller.py
@@ -0,0 +1,231 @@
+from flask import Flask,render_template,request,url_for,redirect
+from flask import current_app as app
+from models.models import *
+from datetime import date,time,datetime,timedelta
+
+
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+############################################## common fuctions for admin and user ####################################################
+
+@app.route('/admin/<name>')
+def admin_dashboard(name):
+    subjects = get_subjects()
+    return render_template("admin_dashboard.html",subjects=subjects,name=name)
+
+@app.route('/user/<name>')
+def user_dashboard(name):
+    return render_template("user_dashboard.html",name=name)
+
+
+@app.route('/quiz_dashboard/<name>')
+def quiz_dashboard(name):
+    quizs = get_quizs()
+    return  render_template("quiz_dashboard.html",name=name,quizs=quizs)
+
+# #################################################### login ########################################################
+
+@app.route('/login',methods=["GET","POST"])
+def login():
+    if request.method=="POST":
+        uname= request.form.get("email")
+        pwd=request.form.get("pwd")
+        usr=User.query.filter_by(email=uname,password=pwd).first()
+        if usr and usr.role==0:
+            return redirect(url_for("admin_dashboard",name=usr.fullname))
+        if usr and usr.role==1:
+            return redirect(url_for("user_dashboard",name=usr.fullname))
+        return render_template("login.html",msg="wrong credentials!!")
+    return render_template("login.html")
+
+
+# ################################################### signup #############################################################
+
+@app.route('/signup',methods=["GET","POST"])
+def signup():
+    min_dob = datetime.today().date() - timedelta(days=18 * 365)
+    if request.method=="POST":
+        uname= request.form.get("email")
+        pwd=request.form.get("pwd")
+        fullname=request.form.get("fullname")
+        qual=request.form.get("qual")
+        dob=request.form.get('dob')
+        if not uname or not pwd or not fullname or not qual or not dob:
+            return render_template("signup.html",msg="fill all inputs properly")
+        year, month, day = map(int, dob.split('-'))
+        date_obj = date(year, month, day)
+        if date_obj >= min_dob:
+            return render_template('signup.html',msg="age should be greatr then 18 years")
+        # **Check if any field is empty**
+        usr=User.query.filter_by(email=uname).first()
+        if usr:
+            return render_template('signup.html',msg="user alredy existed")
+        new_usr=User(email=uname,password=pwd,fullname=fullname,qualification=qual,DOB=date_obj)
+        db.session.add(new_usr)
+        db.session.commit()
+        return render_template('login.html',msg="Registration done successfully")
+    return render_template("signup.html")
+
+
+
+
+#  #########################################     adding a subject  #############################################
+
+@app.route('/add_subject/<name>',methods=["GET","POST"])
+def add_subject(name):
+    if request.method=="POST":
+        sub_name = request.form.get("name")
+        sub_description = request.form.get("description")
+        if not sub_name:
+            return render_template("add_subject.html",name=name,msg="Subject name cant be empty")
+        new_subj = Subject(name=sub_name,description=sub_description)
+        db.session.add(new_subj)
+        db.session.commit()
+        return redirect(url_for("admin_dashboard",name=name))
+    return render_template("add_subject.html",name=name)
+
+
+# ###########################################  editing of subject  #####################################################
+@app.route('/edit_subject/<id>/<name>',methods=["GET","POST"])
+def edit_subject(id,name):
+    subj = get_subject(id)
+    if request.method=="POST":
+        new_sub_name = request.form.get("name")
+        new_sub_description = request.form.get("description")
+        if not new_sub_name:
+            return render_template("edit_subject.html",subject=subj,name=name,msg="subject name cant be empty")
+        subj.name = new_sub_name
+        subj.description = new_sub_description
+        db.session.commit()
+        return redirect(url_for("admin_dashboard",name=name))
+    return render_template("edit_subject.html",subject=subj,name=name)
+
+# ####################################       delete of subject   ################################################
+
+@app.route('/delete_subject/<id>/<name>',methods=["GET","POST"])
+def delete_subject(id,name):
+    subj= get_subject(id)
+    db.session.delete(subj)
+    db.session.commit()
+    return redirect(url_for("admin_dashboard",name=name))
+    
+##################################         add of chApter #################################################
+
+@app.route('/add_chapter/<id>/<name>',methods=["GET","POST"])
+def add_chapter(id,name):
+    subject = get_subject(id=id)
+    if request.method=="POST":
+        chap_name=request.form.get("name")
+        chap_description = request.form.get("description")
+        chapt = Chapter.query.filter_by(name=chap_name).first()
+        if chapt:
+            return render_template("add_chapter.html",subject=subject,name=name,msg="chapter name already exist")
+        elif not chap_name:
+            return render_template("add_chapter.html",subject=subject,name=name,msg="chapter name cant be empty")
+        new_chapter = Chapter(name=chap_name,description=chap_description,Subject_id=subject.id)
+        db.session.add(new_chapter)
+        db.session.commit()
+        return redirect(url_for("admin_dashboard",name=name))
+    return render_template("add_chapter.html",subject=subject,name=name)
+
+
+##################################         edit of chApter #################################################
+
+@app.route("/edit_chapter/<id>/<name>",methods=["GET","POST"])
+def edit_chapter(id,name):
+    chap = get_chapter(id)
+    if request.method=="POST":
+        new_chap_name = request.form.get("name")
+        new_chap_description = request.form.get("description")
+        if not new_chap_name:
+            return render_template("edit_chapter.html",chapter=chap,name=name,msg="subject name cant be empty")
+        chap.name = new_chap_name
+        chap.description = new_chap_description
+        db.session.commit()
+        return redirect(url_for("admin_dashboard",name=name))
+    return render_template("edit_chapter.html",chapter=chap,name=name)
+
+
+##################################         delete of chApter #################################################
+
+@app.route("/delete_chapter/<id>/<name>")
+def delete_chapter(id,name):
+    chap=get_chapter(id)
+    db.session.delete(chap)
+    db.session.commit()
+    return redirect(url_for("admin_dashboard",name=name))
+
+
+
+########################################   add quiz #############################################
+
+@app.route('/add_quiz/<id>/<name>',methods=["GET","POST"])
+def add_quiz(id,name):
+    today = datetime.today().date()
+    chap = get_chapter(id)
+    if request.method=="POST":
+        date_of_quiz = request.form.get("date_of_quiz")
+        time_duration = request.form.get("time_duration")
+        remarks = request.form.get("remarks")
+        if not date_of_quiz or not time_duration :
+            return render_template("add_quiz.html",chapter=chap,name=name,msg="Fill all inputs")
+        year, month, day = map(int, date_of_quiz.split('-'))
+        date_obj = date(year, month, day)
+        hour, minute = map(int, time_duration.split(':'))
+        time_obj = time(hour, minute)
+        if date_obj <= today:
+            return render_template("add_quiz.html",name=name,chapter=chap,msg="increase date")
+        new_quiz=Quiz(date_of_quiz=date_obj,time_duration=time_obj,remarks=remarks,Chapter_id=id)
+        db.session.add(new_quiz)
+        db.session.commit()
+        return redirect(url_for("admin_dashboard",name=name))
+    return render_template("add_quiz.html",name=name,chapter=chap)
+
+
+##################################         search #################################################
+
+@app.route("/search/<name>", methods=['GET','POST'])
+def search(name):
+    if request.method=="POST":
+        search_txt=request.form.get("search_txt")
+        by_subj = search_by_subject(search_txt)
+        # by_chap = search_by_chapter(search_txt)
+        if by_subj:
+            return render_template("admin_dashboard.html",name=name,subjects=by_subj)
+        # elif by_chap:
+        #     return render_template("admin_dashboard.html",name=name,subjects=by_chap)
+    return redirect(url_for("admin_dashboard",name=name))
+
+
+def search_by_subject(search_txt):
+    subj = Subject.query.filter(Subject.name.ilike(f"%{search_txt}%")).all()
+    return subj
+
+
+
+
+# ##########################################        support fuctions      ##################################################  
+
+# for getting all subject info for the admin dashboard
+
+def get_subjects():
+    subjects = Subject.query.all()
+    return subjects 
+
+
+def get_quizs():
+    quizs = Quiz.query.all()
+    return quizs 
+    
+def  get_subject(id):
+    subject = Subject.query.filter_by(id=id).first() 
+    return subject
+
+
+def  get_chapter(id):
+    chapter = Chapter.query.filter_by(id=id).first() 
+    return chapter
---
+++ models/models.py
@@ -0,0 +1,59 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+db=SQLAlchemy()
+
+
+class User(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    email=db.Column(db.String,unique=True,nullable=False)
+    password=db.Column(db.Integer,nullable=False)
+    role=db.Column(db.Integer,default=1)
+    fullname=db.Column(db.String,nullable=False)
+    qualification=db.Column(db.String,nullable=False)
+    DOB = db.Column(db.Date(),nullable=False)
+    scores = db.relationship("Score",cascade="all,delete",backref="user",lazy=True)
+
+
+class Subject(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String,unique=True,nullable=False)
+    description=db.Column(db.String)
+    chapters = db.relationship("Chapter",cascade="all,delete",backref="subject",lazy=True)
+    
+
+
+class Chapter(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String,unique=True,nullable=False)
+    description=db.Column(db.String)
+    Subject_id = db.Column(db.Integer,db.ForeignKey("subject.id"),nullable=False)
+    quizes = db.relationship("Quiz",cascade="all,delete",backref="chapter",lazy=True)
+    
+
+class Quiz(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    Chapter_id = db.Column(db.Integer,db.ForeignKey("chapter.id"),nullable=False)
+    date_of_quiz = db.Column(db.Date(),nullable=False)
+    time_duration = db.Column(db.Time(),nullable =False)
+    remarks=db.Column(db.String)
+    scores= db.relationship("Score",cascade="all,delete",backref="quiz",lazy=True)
+    questions = db.relationship("Question",cascade="all,delete",backref="quiz",lazy=True)
+    
+class Score(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    quiz_id = db.Column(db.Integer,db.ForeignKey("quiz.id"),nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey("user.id"),nullable=False)
+    time_stamp = db.Column(db.Integer,nullable=False)
+    total_scored = db.Column(db.Integer,nullable=False)
+
+class Question(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    quiz_id = db.Column(db.Integer,db.ForeignKey("quiz.id"),nullable=False)
+    question_statement = db.Column(db.String,nullable=False)
+    option1 = db.Column(db.String,nullable=False)
+    option2 = db.Column(db.String,nullable=False)
+    option3 = db.Column(db.String,nullable=False)
+    option4 = db.Column(db.String,nullable=False)
+    correct_option = db.Column(db.Integer,nullable=False)
